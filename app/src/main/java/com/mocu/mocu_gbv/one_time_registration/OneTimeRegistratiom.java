30692a9a6a83d3662feaa15c316591008a800374
Hp <fujoprof@gmail.com>
2024-03-18 14:20:17 +0300

    18.03.2024 v1

---
+++ app/src/main/java/com/mocu/mocu_gbv/one_time_registration/OneTimeRegistratiom.java
@@ -0,0 +1,16 @@
+package com.mocu.mocu_gbv.one_time_registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mocu.mocu_gbv.R;
+
+public class OneTimeRegistratiom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_time_registratiom);
+    }
+}
