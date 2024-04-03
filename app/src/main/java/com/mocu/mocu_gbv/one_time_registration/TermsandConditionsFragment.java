a31e21ffd1c1925559eb46653fd3f206db4d9c43
Hp <fujoprof@gmail.com>
2024-04-03 16:23:06 +0300

    03.04.2024

--- app/src/main/java/com/mocu/mocu_gbv/one_time_registration/TermsandConditionsFragment.java
+++ app/src/main/java/com/mocu/mocu_gbv/one_time_registration/TermsandConditionsFragment.java
@@ -61,6 +61,6 @@ public class TermsandConditionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_termsand_conditions, container, false);
+        return inflater.inflate(R.layout.fragment_terms_and_conditions, container, false);
     }
 }
